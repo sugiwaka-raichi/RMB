7b2159daac1151c2f6477c7338df8a73e3241866
sugiwaka-raichi <72175300+sugiwaka-raichi@users.noreply.github.com>
2020-10-13 14:29:44 +0900

    モンスター元クラスの攻撃仮想関数とモンスターの種族値を受け取る関数を実装

---
+++ RMB/Assets/Monster/MonsterBase.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBase : MonoBehaviour
+{
+    //===============================
+    //モンスターの属性
+    //===============================
+    public enum MONSTER_TYPE
+    {
+        MT_WARTER = 0,
+        MT_FIRE,
+        MT_PLANT
+    }
+
+    private int type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //================================
+    //攻撃の仮想関数
+    //================================
+    virtual public void Attack()
+    {
+        Debug.Log("attack");
+    }
+    
+    //================================
+    //モンスターの属性値を取得する
+    //================================
+    public int GetMonsterType()
+    {
+        return type;
+    }
+    
+
+}
