e246030ac074af18eae53d0988818e847e4655b9
sugiwaka-raichi <72175300+sugiwaka-raichi@users.noreply.github.com>
2020-10-13 11:36:44 +0900

    キーマネージャとキーコンフィグ完成

---
+++ RMB/Assets/Config/KeyConfig/KeyConfig.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class KeyConfig : MonoBehaviour
+{
+    const KeyCode release = KeyCode.Escape;     //入力処理中止の際使うキーを設定
+
+    [SerializeField]
+    private Text keyText;       //設定されているキーを表示
+    bool inputFlg = false;      //入力待ちかどうかを判断する
+    string keyName;             //登録するkeyの名前
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Keyの変更
+        if (inputFlg)
+        {
+            //キーを取得
+            KeyCode keyCode = KeyManager.KeyInput();
+            if (keyCode == 0)
+            {
+                //入力されていなければ何もせず返す
+                return;
+            }
+            else if(keyCode == release)
+            {
+                //入力待ちを解除するキーだった場合
+                inputFlg = false;       //入力待ちをやめる
+                return;
+            }
+            else
+            {
+                //入力されていれば以下
+                Debug.Log("入力されたキー:" + keyCode);
+                KeyManager.SetKey(keyName, keyCode);    //入力されたキーを設定する
+
+                keyText.text = keyCode.ToString();      //入力されたキーを表示する
+                inputFlg = false;                       //入力待ちを解除する
+                
+            }
+        }
+    }
+
+    //=======================================================
+    //入力待ちの開始
+    //=======================================================
+    public void StartKeyInput(string _keyName)
+    {
+        inputFlg = true;
+
+        keyName = _keyName;     //扱うkeyを渡す
+        //keynameが登録されていなければ登録する
+        if (KeyManager.CheckKeyType(keyName))
+        {
+            //登録されている
+        }
+        else
+        {
+            //されていないので登録する
+            KeyManager.AddKey(keyName);
+        }
+    }
+}
---
+++ RMB/Assets/Config/KeyConfig/KeyManager.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class KeyManager : MonoBehaviour
+{
+    public enum KEY_MANAGER_MES
+    {
+        KMM_ADD = 0,
+        KMM_SET,
+        KMM_DELETE,
+        KMM_ERROR
+    }
+
+    private static Dictionary<string, KeyCode> keyConfig = new Dictionary<string, KeyCode>();        //key一覧
+    private readonly string configFilePath;                     //コンフィグファイルのファイルパス
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //========================================
+    //入力状態を取得する関数群
+    //========================================
+    public static bool GetKey(string _key)
+    {
+        if (CheckKeyType(_key))
+        {
+            return Input.GetKey((keyConfig[_key]));
+        }
+        return false;
+    }
+    public static bool GetKeyDown(string _key)
+    {
+        if (CheckKeyType(_key))
+        {
+            return Input.GetKeyDown((keyConfig[_key]));
+        }
+        return false;
+    }
+    public static bool GetKeyUp(string _key)
+    {
+        if (CheckKeyType(_key))
+        {
+            return Input.GetKeyUp(keyConfig[_key]);
+        }
+        return false;
+    }
+
+    //=========================================
+    //使用するkeyを追加で記録する関数
+    //ArgumentExceptionが発生した場合
+    //すでにkeyが登録されている
+    //=========================================
+    public static void AddKey(string _keyName)
+    {
+        keyConfig.Add(_keyName, 0);     //keyだけを追加
+    }
+
+    public static void AddKey(string _keyName,KeyCode _key)
+    {
+        keyConfig.Add(_keyName, _key);      //keyとvalue両方設定
+    }
+
+    //=======================================================
+    //使用するkeyが登録されていなければ追加
+    //されていれば設定する関数
+    //Keyが重複してしまうので非推奨
+    //=======================================================
+    public static int AddSet(string _keyName,KeyCode _key)
+    {
+        //使用するキーが登録されているか調べる
+        if (CheckKeyType(_keyName))
+        {
+            //登録されていれば
+            SetKey(_keyName, _key);
+            return (int)KEY_MANAGER_MES.KMM_SET;
+        }
+        else
+        {
+            //登録されていなければ
+            AddKey(_keyName, _key);
+            return (int)KEY_MANAGER_MES.KMM_ADD;
+        }
+    }
+
+    //=======================================================
+    //使用するkeyを決められたkeyに設定する関数
+    //KeyNotFoundExceptionがでたらkeyが生成されていない
+    //=======================================================
+    public static void SetKey(string _keyName,KeyCode _key)
+    {
+        keyConfig[_keyName] = _key;
+    }
+
+    //========================================================
+    //入力されたkeyを取得する関数
+    //========================================================
+    public static KeyCode KeyInput()
+    {
+        //そもそもkeyが押されているか確認
+        if (Input.anyKeyDown)
+        {
+            //押されていればkeyの状態配列から一つずつcodeに取り出す
+            foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
+            {
+                //押されたkeyと一致すれば押されたkey
+                if (Input.GetKeyDown(code))
+                {
+                    return code;        //入力されたkeyを返す
+                }
+            }
+        }
+        return KeyCode.None;            //見つからなかった
+    }
+
+    //====================================================
+    //keyが登録されているかどうかを調べる
+    //====================================================
+    public static bool CheckKeyType(string _keyName)
+    {
+        if (keyConfig.ContainsKey(_keyName))
+        {
+            //使われていればtrue
+            return true;
+        }
+        else
+        {
+            //使われていなければfalse
+            return false;
+        }
+    }
+
+    //=====================================================
+    //設定したkeyを保存する処理
+    //=====================================================
+    private void SaveKey()
+    {
+        
+    }
+
+    //=====================================================
+    //設定したkeyを読込む処理
+    //=====================================================
+    private void LoadKey()
+    {
+
+    }
+
+}
