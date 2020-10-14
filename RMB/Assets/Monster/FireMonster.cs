d7aea646f3394b143fcf8eef56830ad918f53ccf
sugiwaka-raichi <72175300+sugiwaka-raichi@users.noreply.github.com>
2020-10-14 11:16:35 +0900

    火属性のモンスター仮処理クラス(目安用)

--- RMB/Assets/Monster/FireMonster.cs
+++ RMB/Assets/Monster/FireMonster.cs
@@ -1,21 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MonobitEngine;
 
 public class FireMonster : MonsterBase
 {
+    [SerializeField]
+    GameObject attackObj;     //攻撃時に使うオブジェクトを格納
+    [SerializeField]
+    GameObject diffenceObj;     //防御時に使うオブジェクトを格納
+
+    void Start()
+    {
+        type = (int)MONSTER_TYPE.MT_FIRE;       //火属性のモンスターであることを示す
+    }
+
     //===========================
     //攻撃関数
     //===========================
     public override void Attack()
     {
-        Debug.Log("fire");
-        base.Attack();      //アタックの最低限の処理
+        Debug.Log("fire:" + type);
+        base.Attack();      //攻撃時共通の処理があれば
 
-        //***********************************************************
-        //エフェクトマネージャにエフェクト生成処理を求めるとか
-        //当たり判定の生成とか
-        //***********************************************************
+        //ToDo:詳細な攻撃処理は以下に記述
+        GameObject gameObject = MonobitNetwork.Instantiate(attackObj.name, transform.position, Quaternion.identity, 0);
 
     }
+
+    //===========================
+    //防御関数
+    //===========================
+    public override void Deffence()
+    {
+        Debug.Log("fireDiffence");
+        base.Deffence();
+
+        //ToDo:詳細な防御処理は以下に記述
+        GameObject gameObject = MonobitNetwork.Instantiate(diffenceObj.name, transform.parent.transform.position, Quaternion.identity, 0);
+
+    }
+
+
 }
