2c98419704c27c2512b13d0b5128433793954760
SatoSho <satosho990629gmail.com>
2020-10-15 11:51:36 +0900

    モンスター１種類のみをランダム生成、同じ種類が連続しないように

--- RMB/Assets/Script/GManager.cs
+++ RMB/Assets/Script/GManager.cs
@@ -14,31 +14,33 @@ public class GManager : MonobitEngine.MonoBehaviour
 
     // モンスターのPrefab情報を格納
     [SerializeField] private GameObject[] monsterPrefab = null;
-
     // モンスターのPrefabと生成位置、回転などを格納する配列
     [SerializeField] private GameObject[] monsterArray = null;
 
     // モンスターカウント用変数
     static private int monsterCount = 0;
-
     // モンスター上限
-    const int MONSTER_MAX = 20;
+    private const int MONSTER_MAX = 20;
+    // モンスターの最小数と最大数
+    [SerializeField] private int monsterMin = 0, monsterMax = 3;
+    private int Temp;               // ランダム変数格納用
+    private int TempStrage;         // 情報格納用
 
-    // タイマ
-    [SerializeField] private float timeGenerate = 10;       // 生成のディレイタイム指定
-    private float timeElapsedGenerate;      // 経過時間カウント用
-
-    // 生成する敵のｘ座標の幅(最小値、最大値)
+    // 生成するモンスターのｘ座標の幅(最小値、最大値)
     [Header("Set X Position Min and Max")]
     [SerializeField] private float xMinPos = -10.0f, xMaxPos = 10.0f;
 
-    // 生成する敵のｙ座標設定(ｙはランダムじゃなくていい)
+    // 生成するモンスターのｙ座標設定(ｙはランダムじゃなくていい)
     [Header("Set Y Position Max")]
     [SerializeField] private float yMaxPos = 10.0f;
 
-    // 生成する敵のｚ座標の幅(最小値、最大値)
+    // 生成するモンスターのｚ座標の幅(最小値、最大値)
     [Header("Set Z Position Min and Max")]
     [SerializeField] private float zMinPos = -10.0f, zMaxPos = 10.0f;
+
+    // タイマ
+    [SerializeField] private float timeGenerate = 5;       // 生成のディレイタイム指定
+    private float timeElapsedGenerate;                      // 経過時間カウント用
 
     // スタートフラグ
     private bool bStart = false;
@@ -62,13 +64,13 @@ public class GManager : MonobitEngine.MonoBehaviour
     // スタート
 
     /*============================= Start =============================*/
-    //void Start()
-    //{
+    protected void Start()
+    {
 
-    //}
+    }
 
     /*============================= Update =============================*/
-    void Update()
+    protected void Update()
     {
         // ホストじゃなければ返す
         if (!MonobitNetwork.isHost)
@@ -80,15 +82,17 @@ public class GManager : MonobitEngine.MonoBehaviour
         ChangeScene();          // シーン遷移
         CreateMonster();
 
-        // ネットワークの部分が完成次第bStartフラグをtrueにして、モンスター生成処理を開始する
+        // ネットワークの部分が完成次第bStartフラグをつかったモンスター生成を開始する処理を追加
         //if(bStart == true)
         //{
         //    CreateMonster();        // モンスター生成
         //}
+
+        // ゲーム中のプレイヤーの状態を取得する処理
     }
 
     /*============================= SceneChange =============================*/
-    void ChangeScene()
+    private void ChangeScene()
     {
         // タイトル　GetActiveScene：現在アクティブなシーンを取得
         if (SceneManager.GetActiveScene().name == "TitleSample" && Input.GetMouseButtonDown(0))
@@ -111,38 +115,65 @@ public class GManager : MonobitEngine.MonoBehaviour
 
     /*============================= CreateMonster =============================*/
     // モンスター生成の処理
-    void CreateMonster()
+    private void CreateMonster()
     {
        if (timeElapsedGenerate >= timeGenerate)          // timeOutに指定した秒数に達すれば生成
        {
+           // 三体同時生成
            // monsterArray配列の長さを調べて格納
-           for (int i = 0; i < monsterArray.Length; i++)
-           {
-               // モンスターを上限まで生成
-               if (monsterCount < MONSTER_MAX)
-               {
-                   monsterArray[i] = MonobitNetwork.Instantiate(monsterPrefab[i].name, GetRandomPosition(), Quaternion.identity, 0) as GameObject;
-                   monsterCount++;     // モンスターの数加算
-               }
-               else
-               {
-                   break;
-               }
-           }
-//           Debug.Log("MonsterCreated");
+           //for (int i = 0; i < monsterArray.Length; i++)
+           //{
+           //    // モンスターを上限まで生成
+           //    if (monsterCount < MONSTER_MAX)
+           //    {
+           //        monsterArray[i] = MonobitNetwork.Instantiate(monsterPrefab[i].name, GetRandomPosition(), Quaternion.identity, 0) as GameObject;
+           //        monsterCount++;     // モンスターの数加算
+           //    }
+           //    else
+           //    {
+           //        break;
+           //    }
+           //}
 
-           timeElapsedGenerate = 0.0f;             // 経過時間リセット
-//           Debug.Log("GenerateTimeReset");
+            // 一種類のみランダム発生(連続して同じものが発生しない)
+            // モンスターを上限まで生成していないかチェック
+            if (monsterCount < MONSTER_MAX)
+            {
+                while (true)
+                {
+                    Temp = Random.Range(monsterMin, monsterMax);
+                    if (Temp != TempStrage)
+                    {
+                        monsterArray[Temp] = MonobitNetwork.Instantiate(monsterPrefab[Temp].name, GetRandomPosition(), Quaternion.identity, 0) as GameObject;
+                        monsterCount++;     // モンスターの数加算
+                        Debug.Log("MonsterCreated" + Temp);
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            TempStrage = Temp;
+            timeElapsedGenerate = 0.0f;             // 経過時間リセット
        }
         // 生成タイマ
         timeElapsedGenerate += Time.deltaTime;      // 1秒ずつ加算
     }
 
     /*============================= DestroyedMonster =============================*/
-    // モンスター破棄の処理 削除したときにカウントしてある数を減らす処理を作る
+    // モンスターカウントを減らす処理
     static void DestroyedMonster()
     {
         monsterCount--;
+    }
+
+    /*============================= GetPlayerStatus =============================*/
+    // プレイヤーの状態を取得、ほかの処理に使用できるようにする
+    protected void GetPlayerStatus()
+    {
+
     }
 
     /*============================= GetRandomPosition =============================*/
