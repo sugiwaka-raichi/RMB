7c235270e4c9c8a3a1ee259c75f119c817f7b4af
山本 正 <72176298+TadashiYamamoto@users.noreply.github.com>
2020-10-21 18:01:47 +0900

    ・ステージシーンにて、HPゲージを仮に作成 ・タイトルシーンにてUIアニメを作ろうとして色々いじった形跡あり（衝突することはないはず）

---
+++ RMB/Assets/Scenes/Stage/HPScripts/GaugeCtrl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GaugeCtrl : MonoBehaviour
+{
+    // 最大値を設定
+    private float myHP = 10.0f;
+    // ゲージのオブジェクトを参照する変数
+    private Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ゲージの役割をさせるオブジェクト情報を取得
+        image = this.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            myHP-=2.0f;
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            myHP = 50.0f;
+        }
+
+        image.fillAmount = myHP/10.0f;
+    }
+}
---
+++ RMB/Assets/UIUX/Scripts/TitleAnim.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleAnim : MonoBehaviour
+{
+    private bool animFlag = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animFlag = false;
+
+        // 初期値設定
+        // transformを取得
+        Transform btnTrans = this.transform;
+        // 座標を取得
+        Vector3 btnPos = btnTrans.localPosition;
+
+        btnPos.x = 350.0f;
+        btnPos.y = -200.0f;
+
+        btnTrans.localPosition = btnPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animFlag)
+        {
+            // transformを取得
+            Transform btnTrans = this.transform;
+
+            // 座標を取得
+            Vector3 btnPos = btnTrans.localPosition;
+
+            btnPos.y += 10.0f;
+            if (btnPos.y > 700.0f)
+            {
+                btnPos.y = 700.0f;
+                animFlag = false;
+            }
+
+            // 座標を設定
+            btnTrans.localPosition = btnPos;
+        }
+    }
+
+    // ボタンが押されたときにアニメーションフラグをON
+    // 他のスクリプトへ反映させるためのゲッタープロパティを実行
+    public void OnFlg()
+    {
+        animFlag = true;
+        Debug.Log(animFlag);
+    }
+
+    // ゲッター
+    public bool GetFlg()
+    {
+        return animFlag;
+    }
+}
