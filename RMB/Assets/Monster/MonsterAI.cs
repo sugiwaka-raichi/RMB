84067291d17a0a865cb592c23b86889e50f12c40
sugiwaka-raichi <72175300+sugiwaka-raichi@users.noreply.github.com>
2020-10-22 16:41:04 +0900

    モンスターのAIスクリプトと各metaファイル

---
+++ RMB/Assets/Monster/MonsterAI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterAI : MonoBehaviour
+{
+    private NavMeshAgent monster;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        monster = GetComponent<NavMeshAgent>();     //コンポーネント取得
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    //=====================================================
+    //プレイヤーが入っている間処理をし続ける
+    //=====================================================
+    private void OnTriggerStay(Collider other)
+    {
+        //プレイヤーであれば
+        if (other.gameObject.tag == "Player")
+        {
+            Vector3 destination = new Vector3();        //移動先ベクトルの設定
+
+            //ベクトルを求める
+            destination.x = (this.gameObject.transform.position.x - other.transform.position.x);
+            destination.z = (this.gameObject.transform.position.z - other.transform.position.z);
+            
+            //求めたベクトルを自身の位置から加算して移動先を設定する
+            monster.destination = transform.position + destination;
+        }
+    }
+}
