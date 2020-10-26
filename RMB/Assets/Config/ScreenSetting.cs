c9088d1299cbe1f90b44347e19840f2ad321597b
sugiwaka-raichi <72175300+sugiwaka-raichi@users.noreply.github.com>
2020-10-26 15:09:15 +0900

    スクリーンを強制的にある設定にするクラスを作成

---
+++ RMB/Assets/Config/ScreenSetting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSetting : MonoBehaviour
+{
+    [SerializeField]
+    int screenWidth = 1024;
+    [SerializeField]
+    int screenHeight = 768;
+    [SerializeField]
+    bool fullScreen = false;
+    [SerializeField]
+    int refreshRate = 60;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //スクリーン情報を設定
+        Screen.SetResolution(screenWidth, screenHeight, fullScreen, refreshRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
