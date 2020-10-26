dbfad87dbdcda2f986391dace1f56a82056b88f2
sugiwaka-raichi <72175300+sugiwaka-raichi@users.noreply.github.com>
2020-10-26 18:18:42 +0900

    モンスターAIをアニメーションできるように変更

--- RMB/Assets/Monster/MonsterAI.cs
+++ RMB/Assets/Monster/MonsterAI.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class MonsterAI : MonoBehaviour
+public class MonsterAI : MonobitEngine.MonoBehaviour
 {
+    public enum ANIM_TYPE
+    {
+        isWalk,            //歩きアニメーション
+        isStop,            //停止アニメーション
+    }
+
+    [SerializeField]
+    private Animator animator;              //アニメーター
+    [SerializeField]
+    private ANIM_TYPE nowAnim;              //現在のアニメーション
+    private bool playerOn = false;          //プレイヤーが範囲にいればtrue
+
     private NavMeshAgent monsterAI;
     private MonsterBase monster;
-
     private bool aiflg = true;     //AIを動かすかどうか
 
     // Start is called before the first frame update
@@ -15,11 +26,26 @@ public class MonsterAI : MonoBehaviour
     {
         monsterAI = GetComponent<NavMeshAgent>();     //AIコンポーネント取得
         monster = GetComponent<MonsterBase>();        //モンスターの情報
+        nowAnim = ANIM_TYPE.isStop;         //停止状態から開始
     }
 
     // Update is called once per frame
     void Update()
     {
+        //プレイヤーが範囲にいれば歩きモーションに変更
+        if (playerOn)
+        {
+            animator.SetBool(ANIM_TYPE.isStop.ToString(), false);
+
+            animator.SetBool(ANIM_TYPE.isWalk.ToString(), true);
+        }
+        else
+        {
+            animator.SetBool(ANIM_TYPE.isStop.ToString(), true);
+
+            animator.SetBool(ANIM_TYPE.isWalk.ToString(), false);
+        }
+
         //プレイヤーに持たれた時
         if (monster.GetCatchFlg())
         {
@@ -49,7 +75,20 @@ public class MonsterAI : MonoBehaviour
 
                 //求めたベクトルを自身の位置から加算して移動先を設定する
                 monsterAI.destination = transform.position + destination;
+                playerOn = true;
             }
+        }
+    }
+
+    //=================================================
+    //プレイヤーが範囲から消えればフラグを折る
+    //=================================================
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerOn = false;
+
         }
     }
 }
