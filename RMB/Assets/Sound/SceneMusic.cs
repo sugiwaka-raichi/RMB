f5735fa2a2b77c1aed74ecf57d45c2c9c12da979
sugiwaka-raichi <72175300+sugiwaka-raichi@users.noreply.github.com>
2020-10-27 14:16:13 +0900

    音声ファイルを追加 各シーンで対応した音を鳴らすスクリプトを実装

---
+++ RMB/Assets/Sound/SceneMusic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMusic : MonoBehaviour
+{
+    bool startMusic = false;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        
+    }
+
+    private void Update()
+    {
+        if (!startMusic && ManageSceneLoader.GetActiveScene() != ManageSceneLoader.SceneType.NetScene.ToString())
+        {
+            switch (ManageSceneLoader.GetSceneType())
+            {
+                case ManageSceneLoader.SceneType.TitleScene:
+                    SoundManager.PlayMusic(SoundData.MUSIC_LIST.Title.ToString());
+                    break;
+                case ManageSceneLoader.SceneType.LobbyScene:
+                    SoundManager.PlayMusic(SoundData.MUSIC_LIST.Lobby.ToString());
+                    break;
+                case ManageSceneLoader.SceneType.StageScene:
+                    SoundManager.PlayMusic(SoundData.MUSIC_LIST.Stage.ToString());
+                    break;
+            }
+            startMusic = true;
+        }
+    }
+
+}
