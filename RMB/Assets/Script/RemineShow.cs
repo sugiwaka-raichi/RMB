d69f8e9939df89e301398e6b9b4b42dd0ca01f7c
UeinuiKakeru <k.ueinui@gmail.com>
2020-10-29 15:02:23 +0900

    残存数表記追加

---
+++ RMB/Assets/Script/RemineShow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RemineShow : MonoBehaviour
+{
+    static Text remineText;
+
+    static Text playersText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        remineText = GameObject.Find("AliveRank").GetComponent<Text>();
+        playersText = GameObject.Find("SetAliveRank").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void SetRemine(int _reminecount, int _playerscount)
+    {
+        remineText.text = _reminecount.ToString();
+        playersText.text = _playerscount.ToString();
+    }
+}
